a602e8244c070e430078b35040589d8b5c2a82d0
bannovdaniil <baddan@mail.ru>
2023-03-11 17:57:45 +0300

    refactoring: убрал лишние файлы.

--- src/main/java/io/ylab/task2/sequences/SequencesTest.java
+++
@@ -1,9 +0,0 @@
-package io.ylab.task2.sequences;
-
-public class SequencesTest {
-  public static void main(String[] args) {
-    SequenceGenerator sequenceGenerator = new SequencesImpl();
-
-    sequenceGenerator.a(10);
-  }
-}
