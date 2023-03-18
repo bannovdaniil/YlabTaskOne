37cf7d52a65ca190fbce34b401582d9baed4fd81
bannovdaniil <baddan@mail.ru>
2023-03-19 00:34:36 +0300

    вернул, родной тест.

--- src/main/java/io/ylab/task3/file_sort/Test.java
+++ src/main/java/io/ylab/task3/file_sort/Test.java
@@ -4,26 +4,22 @@ import java.io.File;
 import java.io.IOException;
 
 public class Test {
-  private final static int COUNT = 375_000_000; //375_000_000;
+  private final static int COUNT = 375_000_000;
 
 
   public static void main(String[] args) throws IOException {
-    GeneratorWithHash generatorWithHash = new GeneratorWithHash();
-
     System.out.println("Generate file.");
-    File dataFile = generatorWithHash.generate("data.txt", COUNT);
+    File dataFile = new Generator().generate("data.txt", COUNT);
 
-    long hashCode = generatorWithHash.getHashCode();
-    System.out.println("HashCode: " + hashCode);
     System.out.print("Check sort: ");
-    System.out.println(new ValidatorWithSizeAndHash(dataFile, COUNT, hashCode).isSorted()); // false
+    System.out.println(new Validator(dataFile).isSorted()); // false
 
-    System.out.println("Sort file.");
     long timeStart = System.currentTimeMillis();
+    System.out.println("Sort file.");
     File sortedFile = new Sorter().sortFile(dataFile);
-    System.out.println("Sort Time: " + (System.currentTimeMillis() - timeStart));
+    System.out.println("time: " + (System.currentTimeMillis() - timeStart));
 
     System.out.print("Check sort: ");
-    System.out.println(new ValidatorWithSizeAndHash(sortedFile, COUNT, hashCode).isSorted()); // true
+    System.out.println(new Validator(sortedFile).isSorted()); // true
   }
 }
